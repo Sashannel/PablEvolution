6f707ffb118c0fcf4619ebc1c22e5fcad3f62759
SashaL_ <143262238+Sashannel@users.noreply.github.com>
2025-08-04 19:26:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ from graphics import *
 
 fps = 60
 
-max_screen_x = 1536
-max_screen_y = 864
+max_screen_x = 1820
+max_screen_y = 980
+
+mutation_chance = 0.1
+mutation_amount = 2
+base_food = 500
+base_cells = 1000
 
 window = GraphWin("Pablo Evolution", max_screen_x, max_screen_y)
 window.setBackground('black')
@@ -18,51 +23,90 @@ running = True
 
 def main():
 
+    frame = 0
+    time1 = int(round(time.time() * 1000))
+    time2 = int(round(time.time() * 1000))
+
+    text = Text(Point(65, 20), "FPS: 0")
+    text.setTextColor("white")
+    text.setSize(25)
+    text.draw(window)
+
     for i in range(100000):
 
-        print("Main.py executed")
-        update()
-        time.sleep(1/fps)
-        print("Updated the program!")
+        update(frame)
+        
 
-Creatures = [creature.Creature(window, max_screen_x, max_screen_y, random.randint(0, max_screen_x), random.randint(0, max_screen_y)) for i in range(10)]
-Foods = [food.Food(window, max_screen_x, max_screen_y) for i in range(300)]
+        time2 = int(round(time.time() * 1000))
 
-def update():
+        if time2 - time1 * 1000 < 1/fps:
 
-    print('update()')
+            time.sleep(1/fps)
+
+        if round(1/(time2 - time1 + 1) * 1000) <= fps:
+            text.setText(f"FPS: {round(1/(time2 - time1 + 1) * 1000)}")
+        else:
+            text.setText(f"FPS: {fps}")
+
+        frame += 1
+
+        time1 = int(round(time.time() * 1000))
+
+Creatures = [creature.Creature(window, max_screen_x, max_screen_y, random.randint(0, max_screen_x),
+                               random.randint(0, max_screen_y)) for i in range(base_cells)]
+Foods = [food.Food(window, max_screen_x, max_screen_y) for i in range(base_food)]
+
+def update(frame):
+
+    if frame % 60 == 0:
+
+        new_food = food.Food(window, max_screen_x, max_screen_y)
+        Foods.append(new_food)
 
     for cell in Creatures[:]:
+
         closestDistance = 10000
         closestID = 0
-        for food in Foods[:]:
-            
-            if math.sqrt((cell.x - food.x) ** 2 + (cell.y - food.y) ** 2) < closestDistance:
 
-                closestDistance = math.sqrt((cell.x - food.x) ** 2 + (cell.y - food.y) ** 2)
-                closestID = Foods.index(food)
-                cell.closestX = food.x
-                cell.closestY = food.y
+        for food_item in Foods[:]:
 
-            if math.sqrt((cell.x - food.x) ** 2 + (cell.y - food.y) ** 2) < 7: #checking if the cell can eat this food
+            if math.sqrt((cell.x - food_item.x) ** 2 + (cell.y - food_item.y) ** 2) < closestDistance:
+
+                closestDistance = math.sqrt((cell.x - food_item.x) ** 2 + (cell.y - food_item.y) ** 2)
+                closestID = Foods.index(food_item)
+                cell.closestX = food_item.x
+                cell.closestY = food_item.y
+
+            if math.sqrt((cell.x - food_item.x) ** 2 + (cell.y - food_item.y) ** 2) < 7:
 
                 cell.food += 600
                 cell.score += 1
-                Foods.remove(food)
-                food.is_Dead = True
-                food.update()
+                Foods.remove(food_item)
+                food_item.is_Dead = True
+                food_item.update()
 
                 if cell.score == 2:
 
                     cell.score = 0
-                    new_x = random.randint(cell.x - 10, cell.x + 10)
-                    new_y = random.randint(cell.y - 10, cell.y + 10)
-                    Creatures.append(creature.Creature(window, max_screen_x, max_screen_y, new_x, new_y))
+
+                    for i in range(random.randint(1, 3)):
+
+                        new_x = random.randint(cell.x - 20, cell.x + 20)
+                        new_y = random.randint(cell.y - 20, cell.y + 20)
+                        new_cell = creature.Creature(window, max_screen_x, max_screen_y, new_x, new_y)
+                        Creatures.append(new_cell)
+                        new_cell.brain.layers = cell.nn.copyLayers()
+
+                        for layer in new_cell.brain.layers:
+
+                            layer.mutate(mutation_chance, mutation_amount, new_cell.brain)
+
 
         if cell.update() == "death":
 
             Creatures.remove(cell)
-            print(cell, "is dead")
+
 
 if __name__ == "__main__":
+
     main()
