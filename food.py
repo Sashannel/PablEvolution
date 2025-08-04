c44bde3746103f75654b339a10bad6e9274b7c79
SashaL_ <143262238+Sashannel@users.noreply.github.com>
2025-08-04 15:13:17 +0200

    Create food.py

---
+++ food.py
@@ -0,0 +1,24 @@
+from graphics import *
+import random
+
+class Food():
+
+    def __init__(self, window, max_x, max_y):
+
+        self.x = random.randint(0, max_x)
+        self.y = random.randint(0, max_y)
+        self.is_Dead = False
+        self.window = window
+        self.max_x = max_x
+        self.max_y = max_y
+        self.shape = Circle(Point(self.x, self.y), 5)
+        self.shape.setFill('red')
+        self.shape.draw(window)
+
+    def update(self):
+
+        if self.is_Dead:
+
+            self.shape.undraw()
+            
+            return "death"
