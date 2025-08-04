88c3b29b60af2730339d8c160e04fec8c581b570
SashaL_ <143262238+Sashannel@users.noreply.github.com>
2025-08-04 15:12:36 +0200

    Update creature.py

--- creature.py
+++ creature.py
@@ -5,12 +5,13 @@ import food
 
 class Creature():
 
-    def __init__(self, window, max_x, max_y):
+    def __init__(self, window, max_x, max_y, x, y):
 
-        self.x = random.randint(0, max_x)
-        self.y = random.randint(0, max_y)
+        self.x = x
+        self.y = y
         self.health = 10
         self.food = 300
+        self.score = 0
         self.is_Starving = False
         self.direction = 4 #0=STOP 1= LEFT 2= RIGHT 3= UP 4= DOWN
         self.velocity = 5
@@ -19,11 +20,8 @@ class Creature():
         self.window = window
         self.max_x = max_x
         self.max_y = max_y
-        self.shape = Circle(Point(self.x, self.y), 5)
-        if self.is_Dead == False:
-            self.shape.setFill('blue')
-        else:
-            self.shape.setFill('black')
+        self.shape = Circle(Point(self.x, self.y), 7)
+        self.shape.setFill('blue')
         self.shape.draw(window)
         print("Initial values:", self.x, self.y)
 
@@ -39,31 +37,37 @@ class Creature():
         elif direction == 1: #LEFT
 
             if self.x - velocity  >= 0:
+
                 self.shape.move(-velocity, 0)
                 self.x = self.x - velocity
                 self.y = self.y
                 print("CELL GOING LEFT!")
             else:
+
                 pass
 
         elif direction == 2: #RIGHT
 
             if self.x + velocity <= self.max_x:
+
                 self.shape.move(velocity, 0)
                 self.x = self.x + velocity
                 self.y = self.y
                 print("CELL GOING RIGHT!")
             else:
+
                 pass
 
         elif direction == 3: #UP
 
             if self.y - velocity >= 0:
+
                 self.shape.move(0, -velocity)
                 self.x = self.x
                 self.y = self.y - velocity
                 print("CELL GOING UP!")
             else:
+
                 pass
 
         elif direction == 4: #DOWN
@@ -73,10 +77,13 @@ class Creature():
                 self.x = self.x
                 self.y = self.y + velocity
                 print("CELL GOING DOWN!")
+
             else:
+
                 pass
 
     def update(self):
+
         if self.food == 0:
             self.is_Starving = True
         if self.food < 0:
@@ -89,10 +96,27 @@ class Creature():
             self.health -= 1
 
         if self.health <= 0:
+
             self.is_Dead = True
             self.shape.undraw()
             return "death"
+        
+        if self.x > self.max_x:
 
-        output = (NN.NN().brain([self.x, self.y, self.health, self.food, self.is_Starving, self.direction]))[0]
+            self.x = self.max_x
+
+        if self.x < 0:
+
+            self.x = 0
+
+        if self.y > self.max_y:
+
+            self.y = self.max_y
+
+        if self.y < 0:
+
+            self.y = 0
+
+        output = (NN.NN().brain([self.x, self.y, self.health, self.food, self.is_Starving, self.direction, self.score]))[0]
         self.direction = round(output) 
         self.move(self.direction, self.x, self.y, self.velocity)
